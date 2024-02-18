aa2870e076d786f454b09882e4bcadf21ffa447d
adrian-parra <adrian.adrianborquez@hotmail.com>
2024-02-18 14:29:37 -0700

    Init server

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 5220
+
+const app = express()
+
+// ? HABILITA EL MANEJO DE SOLICITUDES CORS
+app.use(cors())
+
+// ? ANALIZA LAS SOLICITUDES ENTRANTES CON FORMATO JSON.
+app.use(express.json())
+
+app.listen(PORT)
+console.log('sever en puerto ', PORT)
