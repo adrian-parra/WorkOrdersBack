75736e4bea348bb326edd257fa5a963810cc5e62
adrian-parra <adrian.adrianborquez@hotmail.com>
2024-02-18 16:45:36 -0700

    add feat usuarios
    * add model prisma usuario y nivel de autoridad
    
    * add route para obtener todos los usuarios
    
    * add route para obtener usuarios por su id
    
    * add route para actualizar usuario por su id
    
    * add src/config/responseMessages.js para tener un tipo de respuesta reutilisable
    
    * add migration de usuario y nivel de autoridad

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-import corsOptions from './config/corsOptions.js'
+import corsOptions from './src/config/corsOptions.js'
+
+import userRoute from './src/routes/users/userIndex.js'
 
 const PORT = process.env.PORT || 5220
+const RAIZ_ROUTE = process.env.RAIZ_ROUTE || '/api'
 
 const app = express()
 
@@ -11,6 +14,8 @@ app.use(cors(corsOptions))
 
 // ? ANALIZA LAS SOLICITUDES ENTRANTES CON FORMATO JSON.
 app.use(express.json())
+
+app.use(RAIZ_ROUTE, userRoute)
 
 app.listen(PORT)
 console.log('sever en puerto ', PORT)
