d168197b1bdbc41433c44f48e89f7076e749910f
adrian-parra <adrian.adrianborquez@hotmail.com>
2024-02-18 14:35:47 -0700

    add CORS
    * Se abilito configuracion para solicitudes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import corsOptions from './config/corsOptions.js'
 
 const PORT = process.env.PORT || 5220
 
 const app = express()
 
 // ? HABILITA EL MANEJO DE SOLICITUDES CORS
-app.use(cors())
+app.use(cors(corsOptions))
 
 // ? ANALIZA LAS SOLICITUDES ENTRANTES CON FORMATO JSON.
 app.use(express.json())
